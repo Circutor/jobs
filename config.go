8763834eae6e4b162576906a44f90e979394df1c
Bernat <delgadovidalbernat@gmail.com>
2024-04-29 15:11:25 +0200

    add logger to config server

--- config.go
+++ config.go
@@ -2,6 +2,24 @@ package jobs
 
 import "github.com/hibiken/asynq"
 
+type Logger interface {
+	// Debug logs a message at Debug level.
+	Debug(args ...interface{})
+
+	// Info logs a message at Info level.
+	Info(args ...interface{})
+
+	// Warn logs a message at Warning level.
+	Warn(args ...interface{})
+
+	// Error logs a message at Error level.
+	Error(args ...interface{})
+
+	// Fatal logs a message at Fatal level
+	// and process will exit with status set to 1.
+	Fatal(args ...interface{})
+}
+
 type Config struct {
 	// Concurrency specifies the maximum number of concurrent
 	// workers that can process a task queue.
@@ -10,6 +28,11 @@ type Config struct {
 	// Queues specifies the priority of task queues.
 	// The order of the queue names matter.
 	Queues map[string]int
+
+	// Logger specifies the logger used by the server instance.
+	//
+	// If unset, default logger is used.
+	Logger Logger
 }
 
 // DefaultConfig returns the default configuration.
@@ -28,5 +51,6 @@ func (c *Config) toAsynqConfig() asynq.Config {
 	return asynq.Config{
 		Concurrency: c.Concurrency,
 		Queues:      c.Queues,
+		Logger:      c.Logger,
 	}
 }
